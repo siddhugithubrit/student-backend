8eb4e8a24fd163a33be1fa24b66bd37ff63b3387
Siddhesh Mestry <msiddhesh025@gmail.com>
2026-04-21 21:50:33 +0530

    Fix CORS for Netlify

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
